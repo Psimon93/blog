49b5675e3271305a078aa14f14bcb49e8942285e
Simon Petrovic <simon.petrovic@outlook.com>
2025-04-13 22:24:52 +0200

    prvi dan

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const expressLayout = require("express-ejs-layouts");
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
+app.use(express.static("public"));
+
 //template engine
 app.use(expressLayout);
 app.set("layout", "./layout/main");
 app.set("view engine", "ejs");
 
-app.get("", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/", require("./server/routes/main"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
